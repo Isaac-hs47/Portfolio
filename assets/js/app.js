6497992d8f3f5622bf26aa6600c9e1bffbc90921
IHERRERA <Isaac Herrera>
2024-05-30 23:15:15 -0600

    Portfolio update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,12 @@
 import render_techs from "./modules/techs.js";
 import side_menu from "./modules/navigation.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-  render_techs();
+// document.addEventListener("DOMContentLoaded", () => {
+//   render_techs();
 
-});
+// });
 
 document.addEventListener("click", (e) => {
   side_menu(e);
 });
-
 
